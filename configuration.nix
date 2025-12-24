ac1e32d0aeacd8823672a228705d008fe8692849
Michael <schmiddim@gmx.at>
2025-12-24 09:47:39 +0100

    Add Makefile for flake switch and update

---
+++ configuration.nix
@@ -0,0 +1,129 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+  };
+
+  networking.hostName = "ms-nixos";
+  networking.networkmanager.enable = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  console = {
+    keyMap = "de";
+  };
+
+  users.users.ms = {
+    isNormalUser = true;
+    description = "ms";
+    extraGroups = [ "wheel" "networkmanager" "video" "seat" ];
+    initialPassword = "changeme";
+    home = "/home/ms";
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    htop
+    git
+    gnumake
+    gcc
+    usbutils
+    thunderbird
+    google-chrome
+    jetbrains.idea-ultimate
+    freecad
+    gimp
+    go
+    gotools
+    gh
+    kubectl
+    k9s
+
+    sway
+    swaylock
+    swayidle
+    waybar
+    foot
+    bemenu
+    grim
+    slurp
+    wl-clipboard
+    wtype
+    mako
+  ];
+
+  services.printing.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+  hardware.pulseaudio.enable = false;
+
+  security.polkit.enable = true;
+  services.dbus.enable = true;
+  programs.dconf.enable = true;
+
+  services.seatd.enable = true;
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd sway";
+      user = "greeter";
+    };
+  };
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = [
+      "--write-kubeconfig-mode=644"
+      "--disable=traefik"
+    ];
+  };
+
+  environment.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "1";
+    QT_QPA_PLATFORM = "wayland";
+    XDG_CURRENT_DESKTOP = "sway";
+  };
+
+  hardware.opengl.enable = true;
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    font-awesome
+  ];
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "24.05";
+}
